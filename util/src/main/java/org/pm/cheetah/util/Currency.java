5bf391f7c436694e52e3fee5e4ec7b91e6354fec
permagnusson <permagnusson@hispeed.ch>
2020-09-11 09:04:18 +0200

    Added currency pair class

--- util/src/main/java/org/pm/cheetah/util/Currency.java
+++ util/src/main/java/org/pm/cheetah/util/Currency.java
@@ -1,4 +1,30 @@
 package org.pm.cheetah.util;
 
+/**
+ * Enum representing a currency.
+ *
+ * @author Per Magnusson
+ */
 public enum Currency {
+
+    USD("USD"), CHF("CHF"), SEK("SEK");
+    private final String m_iso;
+
+    /**
+     * Creates a new currency enum.
+     *
+     * @param iso The ISO code
+     */
+    Currency(String iso) {
+        m_iso = iso;
+    }
+
+    /**
+     * Returns the currency ISO code.
+     *
+     * @return A String
+     */
+    public String getISOCode() {
+        return m_iso;
+    }
 }
---
+++ util/src/main/java/org/pm/cheetah/util/CurrencyPair.java
@@ -0,0 +1,41 @@
+package org.pm.cheetah.util;
+
+/**
+ * Class representing a currency pair.
+ *
+ * @author Per Magnusson
+ */
+public class CurrencyPair {
+
+    private final Currency m_ccy1;
+    private final Currency m_ccy2;
+
+    /**
+     * Creates a new instance ccy1 / ccy2.
+     *
+     * @param ccy1 The first (numerator) currency
+     * @param ccy2 The second (denominator) currency
+     */
+    public CurrencyPair(Currency ccy1, Currency ccy2) {
+        m_ccy1 = ccy1;
+        m_ccy2 = ccy2;
+    }
+
+    /**
+     * Returns the first currency.
+     *
+     * @return A Currency
+     */
+    public Currency getCurrency1() {
+        return m_ccy1;
+    }
+
+    /**
+     * Returns the second currency.
+     *
+     * @return A Currency
+     */
+    public Currency getCurrency2() {
+        return m_ccy2;
+    }
+}
