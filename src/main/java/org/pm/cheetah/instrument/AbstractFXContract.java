cf7ee6d0498f37a42d7fd7ad18a6d724a23fc376
permagnusson <permagnusson@hispeed.ch>
2020-09-13 09:32:52 +0200

    Added FX contract interface and abstract class

---
+++ src/main/java/org/pm/cheetah/instrument/AbstractFXContract.java
@@ -0,0 +1,32 @@
+package org.pm.cheetah.instrument;
+
+import org.pm.cheetah.util.CurrencyPair;
+
+import java.util.Date;
+
+/**
+ * Abstract class implementing the FX contract interface.
+ *
+ * @author Per Magnusson
+ */
+public class AbstractFXContract implements FXContract {
+
+    protected CurrencyPair m_pair;
+    protected Date m_tradeDate;
+    protected Date m_valueDate;
+
+    @Override
+    public CurrencyPair getCurrencyPair() {
+        return m_pair;
+    }
+
+    @Override
+    public Date getTradeDate() {
+        return m_tradeDate;
+    }
+
+    @Override
+    public Date getValueDate() {
+        return m_valueDate;
+    }
+}
---
+++ src/main/java/org/pm/cheetah/instrument/FXContract.java
@@ -0,0 +1,35 @@
+package org.pm.cheetah.instrument;
+
+import org.pm.cheetah.util.CurrencyPair;
+
+import java.util.Date;
+
+/**
+ * Interface for implementing FX contracts.
+ *
+ * @author Per Magnusson
+ */
+public interface FXContract {
+
+    /**
+     * Method to get  the currency pair.
+     *
+     * @return The currency pair
+     */
+    public CurrencyPair getCurrencyPair();
+
+    /**
+     * Method to get the trade date.
+     *
+     * @return The trade date
+     */
+    public Date getTradeDate();
+
+    /**
+     * Method to get the value date.
+     *
+     * @return The value date
+     */
+    public Date getValueDate();
+
+}
